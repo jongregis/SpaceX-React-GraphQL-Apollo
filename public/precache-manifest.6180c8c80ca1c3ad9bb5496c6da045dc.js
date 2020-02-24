a80555d95ab665f9fee8b7d401c3ebab197be7de
jonathan gregis <jon.gregis@gmail.com>
2020-02-23 19:52:34 -0600

    build

---
+++ public/precache-manifest.6180c8c80ca1c3ad9bb5496c6da045dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34f6266887e9b08bf0392cc01518b4f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f13ca79f6d972078e6e5",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c8f3563263970b028fc5",
+    "url": "/static/js/2.27843da2.chunk.js"
+  },
+  {
+    "revision": "ee50b52a528cc3fd8da8982fb866d42c",
+    "url": "/static/js/2.27843da2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f13ca79f6d972078e6e5",
+    "url": "/static/js/main.474352de.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
